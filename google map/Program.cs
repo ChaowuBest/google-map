b737fced3cd380d60ed390f1d0c11a665f02f341
chao <wu15553992118@gmail.com>
2022-10-24 17:05:47 +0200

    Add project files.

---
+++ google map/Program.cs
@@ -0,0 +1,64 @@
+﻿using Abc_mart;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace google_map
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "";
+            int n = 0;
+            RequestHelper reqhelper = new RequestHelper();
+            WebHeaderCollection req = new WebHeaderCollection();
+            Program pg = new Program();
+            var locationtxt = File.ReadAllLines(Environment.CurrentDirectory + "\\task.txt");
+            foreach (var i in locationtxt)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    var exactpoision = i.Replace("[", "").Replace("]", "").Split("/");
+                    double[] postion = pg.postion(exactpoision[2]);
+                    string url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + postion[0] + ", " + postion[1] + "&key=AIzaSyDnaZ4UFEbNyvimjzuzSevVFb9qSmkyfh4";
+                    string readtoend = reqhelper.httpRequest(url, "", req, Encoding.UTF8, Encoding.UTF8, isusecookie: false, "arcteryxxjp.myshopify.com", "application/json", "POST", "Mobile/7381 CFNetwork/1335.0.3 Darwin/21.6.0", autoRedirect: false, abcRaffle: false, 9000);
+                    if (readtoend.Contains("premise"))
+                    {
+                        Console.WriteLine("GEO Decode Address From Google Map API "+n+"");
+                        n++;
+                        JObject jo = JObject.Parse(readtoend);
+                        address += jo["results"][0]["formatted_address"].ToString()+"\n";
+                    }
+                }
+            }
+            File.WriteAllText(Environment.CurrentDirectory + "\\address.txt", address);
+        }
+        public double[] postion(string value)
+        {
+            string[] positionstring = value.Split(",");
+            double longitude_double = double.Parse(positionstring[0]);
+            double latitude_double = double.Parse(positionstring[1]);
+            Random ran = new Random();
+            double movevalue = ran.NextDouble(0.00202516381, 0.00405032762);
+            return new double[2] { longitude_double + movevalue, latitude_double + movevalue };
+        }
+    }
+    public static class MehtondExtension
+    {
+        public static double NextDouble(this Random random, double miniDouble, double maxiDouble)
+        {
+            if (random != null)
+            {
+                return random.NextDouble() * (maxiDouble - miniDouble) + miniDouble;
+            }
+            else
+            {
+                return 0.0d;
+            }
+        }
+    }
+}
---
+++ google map/RequestHelper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Web;
+
+namespace Abc_mart
+{
+    class RequestHelper
+    {
+        public CookieCollection sitecookie { get; set; } = new CookieCollection();
+        public string responseurl { get; set; }
+        public WebHeaderCollection wr { get; set; }
+        public string Locationurl { get; set; }
+        public string httpRequest(string url, string info, WebHeaderCollection wc, Encoding encoding, Encoding streamEncoding, bool isusecookie,
+                    string host, string contentype, string method, string userAgent, bool autoRedirect, bool abcRaffle = false, int timeut = 5000)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            byte[] contentByte = streamEncoding.GetBytes(info);
+            req.Timeout = timeut;
+            req.Method = method;
+            req.ContentLength = contentByte.Length;
+            req.Accept = "application/json";
+            req.Headers.Add(wc);
+            req.UserAgent = userAgent;
+            req.AllowAutoRedirect = autoRedirect;
+            if (method != "GET")
+            {
+                req.ContentType = contentype;
+                Stream webstream = req.GetRequestStream();
+                webstream.Write(contentByte, 0, contentByte.Length);
+                webstream.Close();
+            }
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    responseurl = response.ResponseUri.ToString();
+                    wr = response.Headers;
+                    StreamReader readhtmlStream = null;
+                    string readtoend = null;
+                    if (response.ContentEncoding == "gzip")
+                    {
+                        Stream tokenStream = response.GetResponseStream();
+                        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                        readhtmlStream = new StreamReader(new GZipStream(tokenStream, CompressionMode.Decompress), encoding);
+                        readtoend = readhtmlStream.ReadToEnd();
+                    }
+                    else if (response.ContentEncoding == "br")
+                    {
+                        Stream tokenStream = response.GetResponseStream();
+                        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                        readhtmlStream = new StreamReader(new BrotliStream(tokenStream, CompressionMode.Decompress), encoding);
+                        readtoend = readhtmlStream.ReadToEnd();
+                    }
+                    else
+                    {
+                        Stream tokenStream = response.GetResponseStream();
+                        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                        readhtmlStream = new StreamReader(tokenStream, encoding);
+                        readtoend = readhtmlStream.ReadToEnd();
+                    }
+                    return readtoend;
+                }
+
+            }
+            catch (WebException ex)
+            {
+                if (ex.Message.Contains("503"))
+                {
+                    Console.WriteLine("503 Service Unavailable");
+                    return "";
+                }
+                else if (ex.Message.Contains("403"))
+                {
+                    Console.WriteLine("403 Forbidden");
+                    return "";
+                }
+                else if (ex.Message.Contains("500"))
+                {
+                    Console.WriteLine("500 Server Error");
+                    return "";
+                }
+                else if (ex.Message.Contains("504"))
+                {
+                    Console.WriteLine("504 Gateway timeout");
+                    return "";
+                }
+                else if (ex.Message.Contains("502"))
+                {
+                    Console.WriteLine("502 Bad Gateway");
+                    return "";
+
+                }
+                else if (ex.Message.Contains("407"))
+                {
+                    Console.WriteLine("407 Proxy Authentication Required");
+                    return "";
+                }
+                else if (ex.Message.Contains("400"))
+                {
+                    Console.WriteLine("400 Bad Request");
+                    return "";
+
+                }
+                else if (ex.Message.Contains("401"))
+                {
+                    Console.WriteLine("401 Unauthorized");
+                    return "";
+                }
+                else if (ex.Message.Contains("429"))
+                {
+                    Console.WriteLine("429 Too Many Requests");
+                    return "";
+                }
+                else
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    return "";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return "";
+            }
+        }
+    }
+}
